b8b006ce482484e3fce54f962a36302b8449c6fe
SIVLEOL <ejlo@ualberta.ca>
2014-09-22 19:17:41 -0600

    updated files a bit  Please enter the commit messag a bit for your changes. Lines starting

--- src/ca/ualberta/cs/ejlo/todolistforandroid/test/ToDoListTest.java
+++ src/ca/ualberta/cs/ejlo/todolistforandroid/test/ToDoListTest.java
@@ -11,7 +11,7 @@ public class ToDoListTest extends TestCase {
 
 	public void testEmptyToDoList(){
 		ToDoList toDoList = new ToDoList();
-		Collection toDoItems = toDoList.getToDoItems();
+		Collection<ToDoItem> toDoItems = toDoList.getToDoItems();
 		assertTrue("Empty to do list", toDoItems.size() == 0);	
 	}
 	
@@ -35,5 +35,18 @@ public class ToDoListTest extends TestCase {
 		assertTrue("To do list size", toDoItems.size() == 0);
 		assertFalse("To do still contains test item", toDoItems.contains(testItem));
 	}
+	
+	public void testPopToDoItem(){
+		ToDoList toDoList = new ToDoList();
+		String testText = "Get gundam";
+		ToDoItem testItem = new ToDoItem(testText);
+		toDoList.addItem(testItem);
+		ToDoItem tempItem = toDoList.popItem(testItem);
+		Collection<ToDoItem> toDoItems = toDoList.getToDoItems();
+		assertTrue("To do list size", toDoItems.size() == 0);
+		assertTrue("To do still contains test item", tempItem == testItem);
+	}
+	
+	//Remember to test for corner cases, like trying to remove something that does not exist
 	
 }
