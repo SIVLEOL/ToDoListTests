81e5205bee3d57cb693dd4b5453d1e8d77bd9ddb
SIVLEOL <ejlo@ualberta.ca>
2014-09-24 08:27:33 -0600

    Lots of equality testing I guess

--- src/ca/ualberta/cs/ejlo/todolistforandroid/test/ToDoTest.java
+++ src/ca/ualberta/cs/ejlo/todolistforandroid/test/ToDoTest.java
@@ -11,6 +11,19 @@ public class ToDoTest extends TestCase {
 		assertTrue("Item text is not testString", testString == item1.getText());
 		assertTrue("Item text.toString is not equal", item1.toString().equals(item1.getText()));
 	}
-
+	
+	public void testToDoEquality(){
+		String ToDoName = "Get rekt";
+		String ToDoNameC = "Get shrekt";
+		ToDoItem A = new ToDoItem(ToDoName);
+		ToDoItem B = new ToDoItem(ToDoName);
+		ToDoItem C = new ToDoItem(ToDoNameC);
+		assertTrue("ToDoA is not equal ToDoB", A.equals(B));
+		assertTrue("ToDoB is not equal ToDoA", B.equals(A));
+		assertFalse("ToDoA is equal ToDoC", A.equals(C));
+		assertFalse("ToDoB is equal ToDoC", B.equals(C));
+		assertTrue("ToDoA is not equal ToDoA", A.equals(A));
+	}
+	
 }
 
